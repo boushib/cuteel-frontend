cdb97f37584feb9b3b0aebc1d26d31078b8457fe
boushib <boushib01@gmail.com>
2021-07-28 12:05:22 +0100

    adjust button padding

--- components/Button.tsx
+++ components/Button.tsx
@@ -7,7 +7,7 @@ export const Button = styled.button`
   color: #fff;
   background: ${({ theme }) => theme.colors.primary};
   box-shadow: 0 4px 14px 0 ${({ theme }) => theme.colors.primary}5c;
-  padding: 0.8rem 1.5rem;
+  padding: 0.7rem 1.4rem;
   font-size: 15px;
   margin-bottom: 12px;
   cursor: pointer;
