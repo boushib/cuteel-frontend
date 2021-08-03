847a9216244cf1b51e939e14810cc5db2d35567a
boushib <boushib01@gmail.com>
2021-08-03 20:48:19 +0100

    integrate with the tickets API

--- pages/support/index.tsx
+++ pages/support/index.tsx
@@ -1,17 +1,60 @@
 import { Button } from '../../components/Button'
 import Head from '../../components/Head'
+import { useState, useContext } from 'react'
+import api from '../../api'
+import { AuthContext } from '../../store'
+import { AuthState } from '../../models'
 
-const Support = () => (
-  <div className="support page">
-    <Head title="Help &amp; Support" />
-    <div className="container">
-      <h1>Contact us</h1>
-      <input type="text" className="form-control" placeholder="Name" />
-      <input type="text" className="form-control" placeholder="Email" />
-      <textarea className="form-control" placeholder="Message"></textarea>
-      <Button>Send</Button>
+const Support = () => {
+  const [subject, setSubject] = useState('')
+  const [description, setDescription] = useState('')
+  const [isTicketCreated, setIsTicketCreated] = useState(false)
+
+  const { state: authState } = useContext(AuthContext) as { state: AuthState }
+  const { user } = authState
+
+  const createTicket = async () => {
+    try {
+      await api.post('/tickets/create', {
+        subject,
+        description,
+        userId: user?._id,
+      })
+      setIsTicketCreated(true)
+    } catch (error: any) {
+      console.log(error.response.data.error)
+    }
+  }
+
+  return (
+    <div className="support page">
+      <Head title="Help &amp; Support" />
+      <div className="container">
+        <h1>Contact us</h1>
+        {isTicketCreated && (
+          <p>
+            Your ticket has been created! We will get in touch with you soon!
+          </p>
+        )}
+        {!isTicketCreated && (
+          <>
+            <input
+              type="text"
+              className="form-control"
+              onChange={(e) => setSubject(e.target.value)}
+              placeholder="Subject"
+            />
+            <textarea
+              className="form-control"
+              onChange={(e) => setDescription(e.target.value)}
+              placeholder="Description"
+            ></textarea>
+            <Button onClick={createTicket}>Send</Button>
+          </>
+        )}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Support
