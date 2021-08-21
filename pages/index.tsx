849ee9c12ef4f2fb953ed89ca58d45bd3cbb1693
boushib <boushib01@gmail.com>
2021-08-21 19:53:05 +0100

    fix the home page spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from '../components/Head'
 import Hero from '../components/home/Hero'
 
 const Home = () => (
-  <div className="home">
+  <div className="home page" style={{ paddingTop: 0 }}>
     <Head title="Home" />
     <Hero />
   </div>
