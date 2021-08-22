f2abfdd949451f8647e0d856db4b2d5702ee35e0
boushib <boushib01@gmail.com>
2021-08-22 14:29:59 +0100

    add a hero bg image

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -5,7 +5,8 @@ const Hero = () => (
   <HeroContainer>
     <div className="container">
       <HeroHeading>
-        <span>Welcome to</span> Cuteel
+        <span>Stylish essentials for your home</span>Designed to elevate your
+        lifestyle
       </HeroHeading>
       <Link href="/catalog" passHref={true}>
         <Button>Shop now</Button>
@@ -23,16 +24,40 @@ const HeroContainer = styled.div`
   padding: 60px 0;
   display: flex;
   align-items: center;
+  background-image: url('/img/hero.jpg');
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  text-align: center;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(72, 72, 72, 0.5);
+    z-index: 0;
+  }
+  button {
+    position: relative;
+    z-index: 1;
+  }
 `
 
 const HeroHeading = styled.h1`
-  font-size: 36px;
-  font-weight: 600;
+  font-size: 40px;
+  font-weight: 500;
+  color: #fff;
+  position: relative;
+  z-index: 1;
+  line-height: 1.2;
+  max-width: 700px;
   span {
-    font-size: 15px;
+    color: inherit;
+    font-size: 20px;
     font-weight: normal;
     display: block;
-    text-transform: uppercase;
-    letter-spacing: 2px;
+    margin-bottom: 10px;
   }
 `
