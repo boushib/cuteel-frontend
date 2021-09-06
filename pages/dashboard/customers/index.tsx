7a31dadba42fb09a9dac2763e24e064d02807c42
boushib <boushib01@gmail.com>
2021-09-07 00:15:06 +0100

    create customers page

--- pages/dashboard/customers/index.tsx
+++ pages/dashboard/customers/index.tsx
@@ -1,11 +1,68 @@
+import api, { setToken } from '@/api/'
+import { ButtonSmall } from '@/components/Button'
 import Head from '@/components/Head'
+import { DEFAULT_AVATAR } from '@/constants/'
+import { User } from '@/models/'
+import { getImagePath } from '@/utils/'
+import { GetServerSideProps } from 'next'
 
-const Customers = () => (
+const getCustomers = async () => {
+  try {
+    const { data } = await api.get('/customers')
+    return data.users
+  } catch (error: any) {
+    console.log(error.response)
+  }
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  setToken(req.cookies['token'])
+  const customers: Array<User> = await getCustomers()
+  return { props: { customers } }
+}
+
+type Props = { customers: Array<User> }
+
+const Customers: React.FC<Props> = ({ customers }) => (
   <>
     <Head title="Customers" />
     <div className="customers">
       <h2>Customers</h2>
-      <p>No customers yet!</p>
+      {customers.length === 0 && <p>No customers yet!</p>}
+      <table>
+        <thead>
+          <tr>
+            <th>Avatar</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer) => (
+            <tr key={customer._id}>
+              <td>
+                <div
+                  className="avatar"
+                  style={{
+                    backgroundImage: `url('${
+                      customer.avatar
+                        ? getImagePath(customer.avatar)
+                        : DEFAULT_AVATAR
+                    }')`,
+                  }}
+                ></div>
+              </td>
+              <td>{customer.name}</td>
+              <td>{customer.email}</td>
+              <td>
+                <ButtonSmall color="#3f51b5">View</ButtonSmall>
+                <ButtonSmall color="#f44336">Delete</ButtonSmall>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   </>
 )
--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -3,6 +3,7 @@ import { AuthState } from '@/models'
 import { AuthContext } from '@/store/providers'
 import Head from '@/components/Head'
 import { getGreeting } from '@/utils/'
+import { Button } from '@/components/Button'
 
 const Dashboard = () => {
   const { state: authState } = useContext(AuthContext) as { state: AuthState }
@@ -11,9 +12,18 @@ const Dashboard = () => {
     <>
       <Head title="Dashboard" />
       <div className="dashboard">
-        <h1>
-          {getGreeting()} {user?.name}!
-        </h1>
+        <h2>Dashboard</h2>
+        <p style={{ fontSize: 16, marginBottom: 20 }}>
+          {getGreeting()} <b>{user?.name}</b>! How can I help?
+        </p>
+        <div className="btn-group">
+          <Button>View products</Button>
+          <Button>Add products</Button>
+          <Button>Manage orders</Button>
+          <Button>Manage customers</Button>
+          <Button>Manage tickets</Button>
+          <Button>Manage discounts</Button>
+        </div>
       </div>
     </>
   )
