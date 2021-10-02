1fdbf463bbbc6af6d288557266831606f9150c13
boushib <boushib01@gmail.com>
2021-10-02 12:16:45 +0100

    create base checkout steps placeholders

--- pages/checkout/index.tsx
+++ pages/checkout/index.tsx
@@ -1,5 +1,7 @@
+import { Button } from '@/components/Button'
 import Checkmark from '@/icons/Checkmark'
 import { useState } from 'react'
+import styles from './index.module.sass'
 
 const STEPS = [
   'Order details',
@@ -10,10 +12,13 @@ const STEPS = [
 
 const Checkout = () => {
   const [currentStep, setCurrentStep] = useState(1)
+  const handleProceed = () => {
+    setCurrentStep((s) => s + 1)
+  }
   return (
     <div className="order page">
       <div className="container">
-        <h1>Checkout</h1>
+        <h1 className={styles.checkout__heading}>Checkout</h1>
         <div className="form__steps">
           {STEPS.map((s, i) => (
             <div
@@ -30,9 +35,58 @@ const Checkout = () => {
             </div>
           ))}
         </div>
+        {currentStep === 1 && (
+          <CheckoutShippingAddress onProceed={handleProceed} />
+        )}
+        {currentStep === 2 && (
+          <CheckoutPaymentMethod onProceed={handleProceed} />
+        )}
+        {currentStep === 3 && <CheckoutPlaceOrder onProceed={handleProceed} />}
+        {currentStep > 3 && <CheckoutDone />}
       </div>
     </div>
   )
 }
+
+type CheckoutShippingAddressProps = {
+  onProceed: () => void
+}
+
+const CheckoutShippingAddress = ({
+  onProceed,
+}: CheckoutShippingAddressProps) => (
+  <div className="checkout__shippingAddress">
+    <h2>Shipping Address</h2>
+    <Button onClick={onProceed}>Next</Button>
+  </div>
+)
+
+type CheckoutPaymentMethodProps = {
+  onProceed: () => void
+}
+
+const CheckoutPaymentMethod = ({ onProceed }: CheckoutPaymentMethodProps) => (
+  <div className="checkout__paymentMethod">
+    <h2>Payment Method</h2>
+    <Button onClick={onProceed}>Next</Button>
+  </div>
+)
+
+type CheckoutPlaceOrderProps = {
+  onProceed: () => void
+}
+
+const CheckoutPlaceOrder = ({ onProceed }: CheckoutPlaceOrderProps) => (
+  <div className="checkout__placeOrder">
+    <h2>Place Order</h2>
+    <Button onClick={onProceed}>Submit</Button>
+  </div>
+)
+
+const CheckoutDone = () => (
+  <div className="checkout__placeOrder">
+    <h3>Congratulations! Your order is placed successfully!</h3>
+  </div>
+)
 
 export default Checkout
