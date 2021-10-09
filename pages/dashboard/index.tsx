da905b56ae772fd41d59a9168652c27cd147239c
boushib <boushib01@gmail.com>
2021-10-09 18:25:19 +0100

    add some charts

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,14 +1,40 @@
+import { Line, Bar } from 'react-chartjs-2'
 import { useContext } from 'react'
 import { AuthState } from '@/models'
 import { AuthContext } from '@/store/providers'
 import Head from '@/components/Head'
-import { getGreeting } from '@/utils/'
-import { Button } from '@/components/Button'
 import styles from './Overview.module.sass'
 import Shop from '@/icons/Shop'
 import User from '@/icons/User'
 import ShoppingCart from '@/icons/ShoppingCart'
 import Money from '@/icons/Money'
+
+const data = {
+  labels: ['1', '2', '3', '4', '5', '6'],
+  datasets: [
+    {
+      label: '# of Orders',
+      data: [12, 19, 3, 5, 2, 3],
+      fill: false,
+      backgroundColor: 'rgb(239, 83, 80)',
+      borderColor: 'rgba(239, 83, 80, 0.2)',
+    },
+  ],
+}
+
+const options: any = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+}
 
 const Dashboard = () => {
   const { state: authState } = useContext(AuthContext) as { state: AuthState }
@@ -61,6 +87,14 @@ const Dashboard = () => {
               <div className={styles.overview__item__label}>Users</div>
               <div className={styles.overview__item__value}>112</div>
             </div>
+          </div>
+        </div>
+        <div style={{ display: 'flex' }}>
+          <div style={{ width: '48%', height: 400 }}>
+            <Line data={data} options={options} />
+          </div>
+          <div style={{ width: '48%', height: 400, marginLeft: '4%' }}>
+            <Bar data={data} options={options} />
           </div>
         </div>
       </div>
