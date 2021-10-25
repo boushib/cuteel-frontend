926d643d040ef609d0532fd65a49d27694d2b3e8
boushib <boushib01@gmail.com>
2021-10-25 17:06:00 +0100

    do not allowed authenticated regular user to access the dashboard

--- components/dashboard/Dashboard.tsx
+++ components/dashboard/Dashboard.tsx
@@ -2,6 +2,10 @@ import Sidebar from './Sidebar'
 import Navbar from './Navbar'
 import Spinner from '@/components/Spinner'
 import styles from './Dashboard.module.scss'
+import { useContext } from 'react'
+import { AuthState } from '@/models/'
+import { AuthContext } from '@/store/providers'
+import { useRouter } from 'next/router'
 
 type Props = {
   pageProps: any
@@ -9,15 +13,25 @@ type Props = {
   isLoading: boolean
 }
 
-const Dashboard: React.FC<Props> = ({ pageProps, Component, isLoading }) => (
-  <>
-    <Sidebar />
-    <Navbar />
-    <div className={styles.dashboard__content}>
-      {isLoading && <Spinner />}
-      {!isLoading && <Component {...pageProps} />}
-    </div>
-  </>
-)
+const Dashboard: React.FC<Props> = ({ pageProps, Component, isLoading }) => {
+  const { state: authState } = useContext(AuthContext) as { state: AuthState }
+  const { user } = authState
+  const router = useRouter()
+  if (!user) return null
+  if (!user.roles.includes('admin')) {
+    router.replace('/profile')
+    return null
+  }
+  return (
+    <>
+      <Sidebar />
+      <Navbar />
+      <div className={styles.dashboard__content}>
+        {isLoading && <Spinner />}
+        {!isLoading && <Component {...pageProps} />}
+      </div>
+    </>
+  )
+}
 
 export default Dashboard
